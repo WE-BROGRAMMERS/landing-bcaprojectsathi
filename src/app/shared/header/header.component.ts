251b458ada8f9e4c9a4a339df6393e3501946f39
dahalutsab <dlutsab2120@gmail.com>
2025-09-18 20:01:10 +0545

    header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component, Signal } from '@angular/core';
 import { SharedService } from '../../services/shared.service';
 import { CommonModule } from '@angular/common';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
@@ -14,6 +15,16 @@ export class HeaderComponent {
 
   constructor(private sharedService: SharedService) {
     this.headerData = this.sharedService.getBrandInfo();
+  }
+
+  mobileNavActive = false;
+
+  toggleMobileNav() {
+    this.mobileNavActive = !this.mobileNavActive;
+  }
+
+  closeMobileNav() {
+    this.mobileNavActive = false;
   }
 
 }
